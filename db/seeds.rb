ebe5eb798f13f95eb4f999f29184721492017fc8
Dwi Prihandi <dwi_prihandi@yahoo.com>
2017-02-05 20:27:58 +0700

    adding seeds and rename logo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+questions = Question.create([
+  {
+    question: "How many hours in a day?",
+    answer: "24"
+  },
+  {
+    question: "How many seconds in a minute?",
+    answer: "60"
+  },
+  {
+    question: "How many days in a week?",
+    answer: "7"
+  },
+  {
+    question: "How many months in a year?",
+    answer: "12"
+  },
+  {
+    question: "How many hours in 3 days?",
+    answer: "72"
+  },
+  {
+    question: "How many letters are there in the English alphabet??",
+    answer: "26"
+  },
+])
